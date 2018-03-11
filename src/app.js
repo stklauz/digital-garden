ef74aebfb2623dd47ca81cc5dc0064be63225d30
stklauz <claudiacvlho@gmail.com>
2018-03-11 19:05:04 +0000

    ADD: webpack dev server

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ function startApp () {
     var canvas = document.getElementById("app");
     var ctx = canvas.getContext("2d");
 
-    canvas.height = 200;
-    canvas.width = 200;
+    canvas.height = 800;
+    canvas.width = 800;
+    canvas.style.border = "1px solid #BCBCBC";
+
+    
 }
 
 window.onload = startApp;
